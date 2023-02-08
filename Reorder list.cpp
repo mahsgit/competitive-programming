a1f2029e5114c61dba1c7f05b5a8f5a9364581a3
Merbebt <103383720+mahsgit@users.noreply.github.com>
2023-02-09 01:50:57 +0300

    Create Reorder list.cpp

---
+++ Reorder list.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+        
+    ListNode* reverseList(ListNode* head) {
+        ListNode *curr = head, *prev = NULL, *temp = NULL;
+        while(curr){
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr=temp;
+        }
+        return prev;
+    }
+
+    void reorderList(ListNode* head) {
+        if(!head->next) return;
+        ListNode *fwd = head;
+        ListNode *fast_fwd = head;
+        while(fast_fwd and fast_fwd->next){
+            fast_fwd = fast_fwd->next->next;
+            fwd = fwd->next;
+        }
+        ListNode *dummyPtr = reverseList(fwd);
+        ListNode *curr = head;
+        ListNode *curr_next = NULL;
+        ListNode *temp = NULL;
+        while(curr->next && dummyPtr->next){
+            curr_next = curr->next;
+            curr->next = dummyPtr;
+            temp = dummyPtr->next;
+            dummyPtr->next = curr_next;
+            curr = curr_next;
+            dummyPtr = temp;
+        }
+        
+    }
+};
